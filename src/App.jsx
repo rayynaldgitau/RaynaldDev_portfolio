d891f3ace30af34f79a891115ef9b85c165171cb
raynald <raynaldgitau@gmail.com>
2025-04-07 14:13:57 +0300

    finnished adding navbars

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen';
+import {Navbar} from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 import "./index.css";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
 
   return(
     <>
       {!isLoaded &&<LoadingScreen onComplete={() => setIsLoaded(true)} />}
+        <div className={`min-h-screen translation-opacity duration-700 
+          ${isLoaded ? "opacity-100" : "opacity-0"}
+           bg-black text-grey-100`}
+           >
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        </div>
     </>
   );
 }
